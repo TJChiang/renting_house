9c4ea0c9521618ee1dcbc3d9ee6ff3b520faf9e3
TJChiang <tjchiang8419@gmail.com>
2023-09-08 14:50:31 +0800

    設定 json parser 格式

--- services/house591/house_structure.go
+++ services/house591/house_structure.go
@@ -7,6 +7,39 @@ type HouseStructure struct {
 }
 
 type MainData struct {
-	TopData []map[string]interface{} `json:"topData"`
-	Data    []map[string]interface{} `json:"data"`
+	TopData []MainDataElement `json:"topData"`
+	Data    []MainDataElement `json:"data"`
+}
+
+type MainDataElement struct {
+	PostId      int      `json:"post_id"`
+	Title       string   `json:"title"`
+	KindName    string   `json:"kind_name"`
+	Type        string   `json:"type"`
+	Location    string   `json:"location"`
+	SectionName string   `json:"section_name"`
+	StreetName  string   `json:"street_name"`
+	Floor       string   `json:"floor_str"`
+	Room        string   `json:"room_str"`
+	Price       string   `json:"price"`
+	PriceUnit   string   `json:"price_unit"`
+	RoleName    string   `json:"role_name"`
+	Contact     string   `json:"contact"`
+	ImageList   []string `json:"photo_list"`
+	RefreshTime string   `json:"refresh_time"`
+	Community   string   `json:"community"`
+	Area        string   `json:"area"`
+	Cid         string   `json:"cid"`
+	Tag         []Tag    `json:"rent_tag"`
+	IsHurry     int      `json:"hurry"`
+	IsCombine   int      `json:"is_combine"`
+	IsSocial    int      `json:"is_social"`
+	IsVip       int      `json:"is_vip"`
+	Preferred   int      `json:"preferred"`
+	Hit         int      `json:"yesterday_hit"`
+}
+
+type Tag struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
